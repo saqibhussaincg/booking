a9bfbfe333296e08aecd8e047223b531e9b637d0
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-07-17 22:21:53 +0500

    'counter'

---
+++ src/components/header/Header.js
@@ -0,0 +1,154 @@
+import React from 'react'
+
+import { useState } from 'react';
+
+import './header.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faBed, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons';
+
+import { faCalendarDays } from '@fortawesome/free-regular-svg-icons';
+  
+import { DateRange  } from 'react-date-range';
+
+import { format } from 'date-fns';
+
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+const Header = () => {
+
+    const [openDate, setOpenDate] = useState(false)
+
+    const [openOptoions, setOpenOptions] = useState(false);
+
+    const [options, setOptions] = useState({
+        adult: 1,
+        children: 0,
+        room: 1,
+    })
+
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        }
+      ]);
+
+  return (
+    <div className='header'>
+    <div className='headerContainer'>
+        <div className='headerList'>
+
+            <div className='headerListItem active'>
+                <FontAwesomeIcon icon={faBed}/>
+                <span>Stays</span>
+            </div>
+
+            <div className='headerListItem'>
+                <FontAwesomeIcon icon={faPlane}/>
+                <span>Flights</span>
+            </div>
+
+            <div className='headerListItem'>
+                <FontAwesomeIcon icon={faBed}/>
+                <span>Attraction</span>
+            </div>
+
+            <div className='headerListItem'>
+                <FontAwesomeIcon icon={faTaxi}/>
+                <span>Airport Taxis</span>
+            </div>
+
+
+        </div>
+
+        <h1 className='headerTitle'>A Lifetime of discounts? It's Genius</h1>
+
+        <p className='headerDesc'>
+            Get rewarded for your travels - unlock instant savings of 10% or more with a free Booking account
+        </p>
+
+        <button className='headerBtn'>Sign In / Register</button>
+
+        <div className='headerSearch'>
+
+            <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faBed} className='headerIcon'/>
+            <input 
+                type='text'
+                placeholder='where are you going?'
+                className='headerSearchInput'
+            />
+            </div>
+
+            <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faCalendarDays} className='headerIcon'/>
+            <span onClick={() => setOpenDate(!openDate)} className='headerSearchText'>
+            {`
+            ${format(date[0].startDate, "MM/dd/yyy")} 
+            to
+            ${format(date[0].endDate, "MM/dd/yyy")}
+            `}
+            </span>
+            
+            {openDate &&
+                <DateRange
+                editableDateInputs={true}
+                onChange={item => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+            />}
+
+            </div>
+
+            <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faPerson} className='headerIcon'/>
+            <span className='headerSearchText'>
+                {`
+                    ${options.adult} adult . ${options.children} children . ${options.room} room
+                `}
+            </span>
+            <div className='options'>
+                <div className='optionItem'>
+                   <span className='optionText'>Adult</span>
+                   <div className='optionCounter'>
+                   <button className='optionCounterButton'>-</button>
+                   <span optionCounterNumber>1</span>
+                   <button className='optionCounterButton'>+</button>
+                   </div>
+                </div>
+                <div className='optionItem'>
+                   <span className='optionText'>Children</span>
+                   <div className='optionCounter'>
+                   <button className='optionCounterButton'>-</button>
+                   <span optionCounterNumber>0</span>
+                   <button className='optionCounterButton'>+</button>
+                   </div>
+                </div>
+                <div className='optionItem'>
+                   <span className='optionText'>Room</span>
+                   <div className='optionCounter'>
+                   <button className='optionCounterButton'>-</button>
+                   <span optionCounterNumber>1</span>
+                   <button className='optionCounterButton'>+</button>
+                   </div>
+                </div>
+            </div>
+            </div>
+
+            <div className='headerSearchItem'>
+            <button className='headerBtn'>Search</button>
+            </div>
+
+        </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default Header
